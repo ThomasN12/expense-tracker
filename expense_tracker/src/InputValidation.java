1a4bc91d875f84c7646bea16a5b855f1d11a2e10
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-09-27 15:49:50 -0400

    add javadoc comment

--- expense_tracker/src/InputValidation.java
+++ expense_tracker/src/InputValidation.java
@@ -3,10 +3,23 @@ import java.util.List;
 
 public class InputValidation {
     private static final List<String> VALID_CATEGORIES = Arrays.asList("food", "travel", "bills", "entertainment", "other");
+    
+    /**
+     * Validates whether the input amount is between 0 and 1000 or not
+     * 
+     * @param amount the transaction amount to validate
+     * @return true if the amount is greater than 0 and less than 1000
+     */
     public static boolean isValidAmount(double amount) {
       return amount > 0 && amount < 1000;
     }
 
+    /**
+     * Validates whether the input category matches the given categories ("food", "travel", "bills", "entertainment", "other")
+     * 
+     * @param category
+     * @return true if the input category matches the given categories
+     */
     public static boolean isValidCategory(String category) {
       return VALID_CATEGORIES.contains(category.toLowerCase());
     }
